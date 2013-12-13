15160738e363b1894421f801e72d2a0b97ad5e44
stkay <doraaki0@gmail.com>
2013-12-13 17:19:01 +0900

    use yaml

--- rev.rb
+++ rev.rb
@@ -1,3 +1,4 @@
+require 'yaml'
 require 'open-uri'
 require 'nokogiri'
 
@@ -6,48 +7,53 @@ File.open("hoge.csv","w") do |file|
 	file << "評価,レビュー"+"\n"
 end
 
-#初期設定
-product = "YAMAHA+P32D+BU/40b5539567f5b97efaaf4ae9ff323722"
-page = 1 
-pageEnd = 100
-spl = "" 
+#URLリストを読み込み
+products = YAML.load_file('urls.yml')
+products.each do |product|
 
-while page < pageEnd
+	#初期設定
+#	product = "" 
+	page = 1 
+	pageEnd = 100
+	spl = "" 
 
-	#評価とレビュー本文を取得、配列に入れる
-	uri = "http://product.rakuten.co.jp/product/#{product}/review/#{page}"
-	doc = Nokogiri::HTML(open(uri))
-	star = doc.css("span.txtPoint").text.unpack("a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4")
-	#レビュー本文の配列を切るための文字列を指定
-	splitter = doc.css("div.revInfo").text.split(/\n\n/)
-	
-	number = 0
-	while number < splitter.length do
-		splitter[number] = splitter[number].gsub(/\n/,"")
-		spl += splitter[number]
-		if number != splitter.length-1 then 
-			spl += "|"
+	while page < pageEnd
+
+		#評価とレビュー本文を取得、配列に入れる
+		uri = "http://product.rakuten.co.jp/product/#{product}/review/#{page}"
+		doc = Nokogiri::HTML(open(uri))
+		star = doc.css("span.txtPoint").text.unpack("a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4")
+		#レビュー本文の配列を切るための文字列を指定
+		splitter = doc.css("div.revInfo").text.split(/\n\n/)
+
+		number = 0
+		while number < splitter.length do
+			splitter[number] = splitter[number].gsub(/\n/,"")
+			spl += splitter[number]
+			if number != splitter.length-1 then 
+				spl += "|"
+			end
+			number = number + 1
 		end
-		number = number + 1
-	end
-	
-	review = doc.css("div.rpsRevListLeft").text.gsub(/\n|[0-9]人が参考になったと回答/,"").split(/#{spl}/)	
 
-	puts page 
+		review = doc.css("div.rpsRevListLeft").text.gsub(/\n|[0-9]人が参考になったと回答/,"").split(/#{spl}/)	
 
-	#もし評価数とレビュー本文の数が合わない場合は処理を止める
-	if star.length != review.length then
-		break
-	end
+			puts page 
 
-	#CSVに出力
-	num = 0
-	while num < star.length do
-		File.open("hoge.csv","a") do |csv|
-			csv << star[num]+","+review[num]+"\n"
+		#もし評価数とレビュー本文の数が合わない場合は処理を止める
+		if star.length != review.length then
+			break
 		end
-		num = num + 1
-	end
 
-	page = page + 1
+		#CSVに出力
+		num = 0
+		while num < star.length do
+			File.open("hoge.csv","a") do |csv|
+				csv << star[num]+","+review[num]+"\n"
+			end
+			num = num + 1
+		end
+
+		page = page + 1
+	end
 end
