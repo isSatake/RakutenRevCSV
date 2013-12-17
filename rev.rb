bb22e9bedadefb8312e93fbbdebb5c4989336511
stkay <doraaki0@gmail.com>
2013-12-17 12:22:02 +0900

    encoding windows31j

--- rev.rb
+++ rev.rb
@@ -1,10 +1,13 @@
+# encoding:Windows-31J
 require 'yaml'
 require 'open-uri'
 require 'nokogiri'
 
 #CSVの1行目を記述
+index = "star,age,sex,review"
+index.encode("Windows-31J")
 File.open("hoge.csv","w") do |file|
-	file << "評価,レビュー"+"\n"
+	file << index+"\n" 
 end
 
 #URLリストを読み込み
@@ -12,44 +15,46 @@ products = YAML.load_file('urls.yml')
 products.each do |product|
 
 	#初期設定
-#	product = "" 
 	page = 1 
 	pageEnd = 100
-	spl = "" 
 
+	#評価とレビュー本文を取得、配列に入れる
 	while page < pageEnd
-
-		#評価とレビュー本文を取得、配列に入れる
+		starArray = Array[] 
+		ageArray = Array[]
+		sexArray = Array[]
+		reviewArray = Array[]
 		uri = "http://product.rakuten.co.jp/product/#{product}/review/#{page}"
 		doc = Nokogiri::HTML(open(uri))
-		star = doc.css("span.txtPoint").text.unpack("a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4")
-		#レビュー本文の配列を切るための文字列を指定
-		splitter = doc.css("div.revInfo").text.split(/\n\n/)
+		rpsRevListLeft = doc.css(".rpsRevListLeft")
+		rpsRevListLeft.each do |content|	
+			starArray << content.css("span.txtPoint").text
+			reviewArray << content.css(".revTxt").text.gsub(/\n/,"")
+			ageArray << content.css(".revAges").text.slice(1,3)
+			sexArray << content.css(".revAges").text.slice(5,2)
 
-		number = 0
-		while number < splitter.length do
-			splitter[number] = splitter[number].gsub(/\n/,"")
-			spl += splitter[number]
-			if number != splitter.length-1 then 
-				spl += "|"
-			end
-			number = number + 1
 		end
 
-		review = doc.css("div.rpsRevListLeft").text.gsub(/\n|[0-9]人が参考になったと回答/,"").split(/#{spl}/)	
+		#nilを文字列に置換
+		for int in 0..30 do
+			unless ageArray[int] then
+				ageArray[int] = "nothing" 
+				sexArray[int] = "nothing"	
+			end
+		end
 
-			puts page 
+		puts page 
 
 		#もし評価数とレビュー本文の数が合わない場合は処理を止める
-		if star.length != review.length then
+		if starArray.length == 0 && reviewArray.length == 0 then
 			break
 		end
 
 		#CSVに出力
 		num = 0
-		while num < star.length do
-			File.open("hoge.csv","a") do |csv|
-				csv << star[num]+","+review[num]+"\n"
+		while num < starArray.length do
+			File.open("hoge.csv","a:Windows-31J") do |file|
+				file << starArray[num]+","+ageArray[num]+","+sexArray[num]+","+reviewArray[num]+"\n"
 			end
 			num = num + 1
 		end
